46822d84338a0d4c33a89efbc9d6ae702e9fcc39
Procyonae <45432782+Procyonae@users.noreply.github.com>
2024-01-26 18:34:35 +0000

    skipped_members fix

--- src/item_factory.cpp
+++ src/item_factory.cpp
@@ -4202,8 +4202,10 @@ void Item_factory::load_basic_info( const JsonObject &jo, itype &def, const std:
         def.extend_description = false;
     }
 
-    if( jo.read( "description_prepend", def.description_prepend ) ||
-        jo.read( "description_append", def.description_append ) ) {
+    if( jo.read( "description_prepend", def.description_prepend ) ) {
+        def.extend_description = true;
+    }
+    if( jo.read( "description_append", def.description_append ) ) {
         def.extend_description = true;
     }
 
